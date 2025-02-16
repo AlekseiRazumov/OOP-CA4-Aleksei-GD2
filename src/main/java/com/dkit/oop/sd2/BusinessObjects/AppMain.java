ffd9081f291126ca5a4673b8890c25b842644127
AlekseiRazumov <d00264319@student.dkit.ie>
2025-02-16 22:32:31 +0300

    Update AppMain.java

--- src/main/java/com/dkit/oop/sd2/BusinessObjects/AppMain.java
+++ src/main/java/com/dkit/oop/sd2/BusinessObjects/AppMain.java
@@ -1,22 +1,6 @@
 package com.dkit.oop.sd2.BusinessObjects;
 
-/** OOP Jan 2025
- * This AppMain demonstrates the use of a Data Access Layer
- * to separate Business logic from Database specific logic.
- * It uses:
- * Data Access Objects (DAOs) to implement the logic required to access a database.
- * Data Transfer Objects (DTOs), to transfer data between layers, and a
- * DAO Interface to define a 'contract' between Business Objects and DAOs.
- *
- * "Use a Data Access Object (DAO) to abstract and encapsulate all
- * access to the data source. The DAO manages the connection with
- * the data source to obtain and store data" Ref: oracle.com
- *
- * Here, we use one DAO per database table.
- *
- * Use the SQL script "CreateUsers.sql" included with this project
- * to create the required MySQL user_database and User table.
- */
+
 
 import com.dkit.oop.sd2.DAOs.MySqlUserDao;
 import com.dkit.oop.sd2.DAOs.UserDaoInterface;
