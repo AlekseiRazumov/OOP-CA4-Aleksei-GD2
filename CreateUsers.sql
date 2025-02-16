b991bfe8fa52c941a79dedac7c495313f6500506
AlekseiRazumov <d00264319@student.dkit.ie>
2025-02-17 00:22:00 +0300

    Making SQL tables

--- CreateUsers.sql
+++ CreateUsers.sql
@@ -1,20 +1,25 @@
--- OOP 2022
 
-DROP DATABASE IF EXISTS `user_database`;
-CREATE DATABASE `user_database`;
-USE `user_database`;
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `USER_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `FIRST_NAME` varchar(50) NOT NULL,
-  `LAST_NAME` varchar(50) NOT NULL,
-  `USERNAME` varchar(20) NOT NULL,
-  `PASSWORD` varchar(20) NOT NULL,
-  PRIMARY KEY  (`USER_ID`)
-  );
 
-INSERT INTO user VALUES (null, "John", "Smith", "smithj", "password"),
-  			(null, "Mary", "Byrne", "byrnem", "password"),
-  			(null, "Frankie", "Cocozza", "cocozzaf", "password"),
-            (null, "Patrick", "Jones", "jonesp", "password"),
-            (null, "John", "Smyth", "smythj", "password");
+DROP DATABASE IF EXISTS `ca4_database`;
+CREATE DATABASE `ca4_database`;
+USE `ca4_database`;
+DROP TABLE IF EXISTS expenses;
+CREATE TABLE expenses(expenseID int NOT NULL AUTO_INCREMENT,
+             title VARCHAR(20),
+             category VARCHAR(50),
+             amount double(10,2),
+             dateIncurred date,
+			 PRIMARY KEY (expenseID));
+CREATE TABLE incomes(incomeID int NOT NULL AUTO_INCREMENT,
+             title VARCHAR(20),
+             amount double(10,2),
+             dateEarned date,
+			 PRIMARY KEY (incomeID));
+
+
+
+INSERT INTO expenses VALUES (1, 'weekly shop', 'groceries', '47.50','2025-01-25' );
+INSERT INTO expenses VALUES (2, 'gym membership', 'fitness', '30.10','2025-02-15' );
+
+INSERT INTO incomes VALUES (1, 'babysitting', '60.05','2025-01-16' );
+INSERT INTO incomes VALUES (2, 'bar work', '100.50','2025-01-12' );
